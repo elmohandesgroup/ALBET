07a1e98cd9f3f235a4b52b3616db464301025563
elmohandesgroup <elmohandesgroup2020@gmail.com>
2026-09-13 11:43:52 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,20 @@
-self.addEventListener('fetch', (event) => {});
+const CACHE_NAME = 'albet-store-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
